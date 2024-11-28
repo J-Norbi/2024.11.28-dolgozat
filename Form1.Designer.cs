a26bc4008675fbd01e4c9f423ee56c9a1d72f2fd
jambo <jambo@LAPTOP-TOR02GNI>
2024-11-28 20:38:13 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace _2024._11._28_dolgozat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.feszErtek = new System.Windows.Forms.Label();
+            this.eredmeny = new System.Windows.Forms.Label();
+            this.akkumulator = new System.Windows.Forms.PictureBox();
+            this.elektron = new System.Windows.Forms.PictureBox();
+            this.balVezetek = new System.Windows.Forms.PictureBox();
+            this.jobbVezetek = new System.Windows.Forms.PictureBox();
+            this.alsoVezetek = new System.Windows.Forms.PictureBox();
+            this.feszszabalyzo = new System.Windows.Forms.PictureBox();
+            this.felsoVezetek2 = new System.Windows.Forms.PictureBox();
+            this.felsoVezetek1 = new System.Windows.Forms.PictureBox();
+            this.korte = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.akkumulator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elektron)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balVezetek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobbVezetek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alsoVezetek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.feszszabalyzo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.felsoVezetek2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.felsoVezetek1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.korte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(366, 122);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(40, 24);
+            this.buttonUp.TabIndex = 8;
+            this.buttonUp.Text = "/\\";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(366, 151);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(40, 24);
+            this.buttonDown.TabIndex = 9;
+            this.buttonDown.Text = "\\/";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            // 
+            // feszErtek
+            // 
+            this.feszErtek.AutoSize = true;
+            this.feszErtek.Location = new System.Drawing.Point(372, 100);
+            this.feszErtek.Name = "feszErtek";
+            this.feszErtek.Size = new System.Drawing.Size(26, 13);
+            this.feszErtek.TabIndex = 10;
+            this.feszErtek.Text = "12V";
+            // 
+            // eredmeny
+            // 
+            this.eredmeny.AutoSize = true;
+            this.eredmeny.Location = new System.Drawing.Point(538, 40);
+            this.eredmeny.Name = "eredmeny";
+            this.eredmeny.Size = new System.Drawing.Size(100, 13);
+            this.eredmeny.TabIndex = 11;
+            this.eredmeny.Text = "Electrons passed: 0";
+            // 
+            // akkumulator
+            // 
+            this.akkumulator.BackgroundImage = global::_2024._11._28_dolgozat.Properties.Resources.akkumulator1;
+            this.akkumulator.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.akkumulator.Location = new System.Drawing.Point(561, 306);
+            this.akkumulator.Name = "akkumulator";
+            this.akkumulator.Size = new System.Drawing.Size(90, 57);
+            this.akkumulator.TabIndex = 2;
+            this.akkumulator.TabStop = false;
+            // 
+            // elektron
+            // 
+            this.elektron.BackColor = System.Drawing.Color.Yellow;
+            this.elektron.Location = new System.Drawing.Point(638, 308);
+            this.elektron.Name = "elektron";
+            this.elektron.Size = new System.Drawing.Size(6, 6);
+            this.elektron.TabIndex = 12;
+            this.elektron.TabStop = false;
+            // 
+            // balVezetek
+            // 
+            this.balVezetek.BackColor = System.Drawing.Color.Red;
+            this.balVezetek.Location = new System.Drawing.Point(172, 146);
+            this.balVezetek.Name = "balVezetek";
+            this.balVezetek.Size = new System.Drawing.Size(2, 158);
+            this.balVezetek.TabIndex = 7;
+            this.balVezetek.TabStop = false;
+            // 
+            // jobbVezetek
+            // 
+            this.jobbVezetek.BackColor = System.Drawing.Color.Blue;
+            this.jobbVezetek.Location = new System.Drawing.Point(640, 142);
+            this.jobbVezetek.Name = "jobbVezetek";
+            this.jobbVezetek.Size = new System.Drawing.Size(2, 164);
+            this.jobbVezetek.TabIndex = 6;
+            this.jobbVezetek.TabStop = false;
+            // 
+            // alsoVezetek
+            // 
+            this.alsoVezetek.BackColor = System.Drawing.Color.Red;
+            this.alsoVezetek.Location = new System.Drawing.Point(172, 304);
+            this.alsoVezetek.Name = "alsoVezetek";
+            this.alsoVezetek.Size = new System.Drawing.Size(400, 2);
+            this.alsoVezetek.TabIndex = 5;
+            this.alsoVezetek.TabStop = false;
+            // 
+            // feszszabalyzo
+            // 
+            this.feszszabalyzo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.feszszabalyzo.Location = new System.Drawing.Point(340, 89);
+            this.feszszabalyzo.Name = "feszszabalyzo";
+            this.feszszabalyzo.Size = new System.Drawing.Size(90, 92);
+            this.feszszabalyzo.TabIndex = 1;
+            this.feszszabalyzo.TabStop = false;
+            // 
+            // felsoVezetek2
+            // 
+            this.felsoVezetek2.BackColor = System.Drawing.Color.Red;
+            this.felsoVezetek2.Location = new System.Drawing.Point(181, 142);
+            this.felsoVezetek2.Name = "felsoVezetek2";
+            this.felsoVezetek2.Size = new System.Drawing.Size(160, 2);
+            this.felsoVezetek2.TabIndex = 4;
+            this.felsoVezetek2.TabStop = false;
+            // 
+            // felsoVezetek1
+            // 
+            this.felsoVezetek1.BackColor = System.Drawing.Color.Blue;
+            this.felsoVezetek1.Location = new System.Drawing.Point(426, 142);
+            this.felsoVezetek1.Name = "felsoVezetek1";
+            this.felsoVezetek1.Size = new System.Drawing.Size(214, 2);
+            this.felsoVezetek1.TabIndex = 3;
+            this.felsoVezetek1.TabStop = false;
+            // 
+            // korte
+            // 
+            this.korte.BackgroundImage = global::_2024._11._28_dolgozat.Properties.Resources.korte;
+            this.korte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.korte.Location = new System.Drawing.Point(140, 72);
+            this.korte.Name = "korte";
+            this.korte.Size = new System.Drawing.Size(67, 84);
+            this.korte.TabIndex = 0;
+            this.korte.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.akkumulator);
+            this.Controls.Add(this.elektron);
+            this.Controls.Add(this.eredmeny);
+            this.Controls.Add(this.feszErtek);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.balVezetek);
+            this.Controls.Add(this.jobbVezetek);
+            this.Controls.Add(this.alsoVezetek);
+            this.Controls.Add(this.feszszabalyzo);
+            this.Controls.Add(this.felsoVezetek2);
+            this.Controls.Add(this.felsoVezetek1);
+            this.Controls.Add(this.korte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.akkumulator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elektron)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balVezetek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobbVezetek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alsoVezetek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.feszszabalyzo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.felsoVezetek2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.felsoVezetek1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.korte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox korte;
+        private System.Windows.Forms.PictureBox feszszabalyzo;
+        private System.Windows.Forms.PictureBox akkumulator;
+        private System.Windows.Forms.PictureBox felsoVezetek1;
+        private System.Windows.Forms.PictureBox felsoVezetek2;
+        private System.Windows.Forms.PictureBox alsoVezetek;
+        private System.Windows.Forms.PictureBox jobbVezetek;
+        private System.Windows.Forms.PictureBox balVezetek;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Label feszErtek;
+        private System.Windows.Forms.Label eredmeny;
+        private System.Windows.Forms.PictureBox elektron;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _2024._11._28_dolgozat
+{
+    public partial class Form1 : Form
+    {
+        Timer electronMoveTimer = new Timer();
+        Timer electronAddTimer = new Timer();
+        int voltSize = 12;
+        int voltSpeed = 0;
+        int iranyX = 0;                 // 1 - jobb; 0 - fel vagy le; -1 - bal
+        int iranyY = -1;                // 1 - le; 0 - jobb vagy bal; -1 - fel
+        int electronEredmeny = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Dolgozat - Electron";
+            voltSpeed = voltSize / 3;
+            Start();
+        }
+        void Start()
+        {
+            electronMoveTimer.Interval = 16;
+            electronAddTimer.Interval = 500;
+            electronAddTimer.Start();
+            electronMoveTimer.Start();
+
+            AddEvents();
+            //electronMoveTimer.Tick += ElectronMove;
+            //electronAddTimer.Tick += AddElectron;
+            AddElectron();
+        }
+        void AddEvents()
+        {
+            buttonUp.Click += UpVolatages;
+            buttonDown.Click += DownVoltages;
+        }
+        void AddElectron()  //object s, EventArgs e
+        {
+            PictureBox electron = new PictureBox();
+            this.Controls.Add(electron);
+            electron.Location = new Point(638, 308);
+            electron.Size = new Size(6, 6);
+            electron.BackColor = Color.Red;
+            electronMoveTimer.Tick += (ss, ee) =>
+            {
+                //  felfelé
+                if (iranyX == 0 && iranyY == -1)
+                {
+                    if (electron.Top > felsoVezetek1.Top + 24 / 3)
+                    {
+                        electron.Top -= 24 / 3;
+                    }
+                    else
+                    {
+                        iranyX = -1;
+                        iranyY = 0;
+                        electron.Top = felsoVezetek1.Top - 2;
+                    }
+                }
+                //  lefelé
+                else if (iranyX == 0 && iranyY == 1)
+                {
+                    if (electron.Top < alsoVezetek.Top - voltSpeed)
+                    {
+                        electron.Top += voltSpeed;
+                    }
+                    else
+                    {
+                        iranyX = 1;
+                        iranyY = 0;
+                        electron.Top = alsoVezetek.Top - 2;
+                    }
+                }
+                // balra
+                else if (iranyX == -1 && iranyY == 0)
+                {
+                    if (electron.Left > feszszabalyzo.Right - 24/3)
+                    {
+                        electron.Left -= 24/3;
+                    }
+                    else if (electron.Left < feszszabalyzo.Right && electron.Left > balVezetek.Left + voltSpeed)
+                    {
+                        electron.Left -= voltSpeed;
+                    }
+                    else
+                    {
+                        iranyX = 0;
+                        iranyY = 1;
+                        electron.Left = balVezetek.Left - 2;
+                    }
+                }
+                // jobbra
+                else if (iranyX == 1 && iranyY == 0)
+                {
+                    if (electron.Left < alsoVezetek.Right - voltSpeed)
+                    {
+                        electron.Left += voltSpeed;
+                    }
+                    else
+                    {
+                        iranyX = 0;
+                        iranyY = -1;
+                        electron.Left = jobbVezetek.Left - 2;
+                        electron.Top = 308;
+                        //electronMoveTimer.Stop();
+                        this.Controls.Remove(electron);
+                        electronEredmeny++;
+                        eredmeny.Text = $"Electrons passed: {electronEredmeny}";
+                        AddElectron();
+                    }
+                }
+            };
+        }
+        void UpVolatages(object s, EventArgs e)
+        {
+            if (voltSize < 24)
+            {
+                voltSize += 1;
+                feszErtek.Text = $"{voltSize}V";
+                voltSpeed = voltSize / 3;
+            }
+        }
+        void DownVoltages(object s, EventArgs e)
+        {
+            if (voltSize > 1)
+            {
+                voltSize -= 1;
+                feszErtek.Text = $"{voltSize}V";
+                voltSpeed = voltSize / 3;
+            }
+        }
+    }
+}
